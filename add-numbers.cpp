ed00c44cbe7afaaf5a3237c49ee608c203b6dd17
NylonEric <51034223+NylonEric@users.noreply.github.com>
2022-05-29 00:27:22 +0100

    add-numbers example

---
+++ add-numbers.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+int main() {
+  int firstNumber, secondNumber, sum;
+  cout << "Enter two integers: ";
+  cin >> firstNumber >> secondNumber;
+  sum = firstNumber + secondNumber;
+  cout << firstNumber << "+ " << secondNumber << " = " << sum << "\n";
+  return 0;
+}
